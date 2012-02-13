9ae4b252949739c0a74d42de157d41f78792d48f
Sean Montgomery <smmontgom@AuVidBot.local>
2012-02-13 01:10:44 -0500

    broken device.platform link to position:fixed fix

--- Android_iParade/assets/www/ios.js
+++ Android_iParade/assets/www/ios.js
@@ -1,8 +1,13 @@
 var floater = function () {
-    console.log('floater');
+    console.debug('floater');
+    var devidePlatform = window.device.platform;
+    console.debug(devidePlatform);
+    if ((device.platform == 'iPhone') || (device.platform == 'iOS')) {
+    console.debug('iOS');
     //console.log(window.pageYOffset + ', ' + (document.documentElement.clientHeight - window.pageYOffset + window.innerHeight));
     document.getElementById('tabs').style.top = (window.pageYOffset) + 'px';
     document.getElementById('tabs').style.bottom = (-window.pageYOffset) + 'px';
+    }
 };
 
 window.onscroll = floater;
