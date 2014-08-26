7a3b9593a87d079ab901dc1fc6659ef0882292d6
BJ Warshaw <benjarwar@gmail.com>
2014-08-26 17:24:03 -0400

    Update rhizome backup

--- rhizome-backup/iParades.php
+++ rhizome-backup/iParades.php
@@ -2,11 +2,6 @@
   if (isset($_POST['latitude']) && isset($_POST['longitude'])){
   	$output = array(
 			array(
-				"name" => "iParade#1",
-				"url" => "http://archive.rhizome.org:8080/lovid/iparade/iparade1/",
-				"location" => array("latitude" => 40.619931, "longitude" => -73.949188, "accuracy" => 300)
-				),
-			array(
 				"name" => "iParade#2",
 				"url" => "http://archive.rhizome.org:8080/lovid/iparade/iparade2/",
 				"location" => array("latitude" => 40.819931, "longitude" => -73.949188, "accuracy" => 300)
