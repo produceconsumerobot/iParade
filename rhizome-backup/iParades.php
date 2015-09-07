06404d69b1c84b8138b8e23c6f5fc4e79dd7ef80
BJ Warshaw <benjarwar@gmail.com>
2015-09-07 00:11:17 -0400

    Add source/text/html files for iParade 3 and update locations

--- rhizome-backup/iParades.php
+++ rhizome-backup/iParades.php
@@ -5,7 +5,12 @@
 				"name" => "iParade#2",
 				"url" => "http://archive.rhizome.org:8080/lovid/iparade/iparade2/",
 				"location" => array("latitude" => 40.819931, "longitude" => -73.949188, "accuracy" => 300)
-				)
+			),
+			array(
+				"name" => "iParade#3",
+				"url" => "http://archive.rhizome.org:8080/lovid/iparade/iparade3/",
+				"location" => array("latitude" => 42.727417, "longitude" => -73.680361, "accuracy" => 300)
+			)
 		);
   }else{
   	$output = "";
